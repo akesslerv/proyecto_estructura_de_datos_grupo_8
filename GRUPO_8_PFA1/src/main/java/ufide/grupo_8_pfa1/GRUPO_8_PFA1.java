cca5b295d540cc057bc31a0f31e6d495d209770f
JCordero60060 <jcordero60060@ufide.ac.cr>
2025-06-09 11:59:21 -0600

    Testeando integracion Netbeans - Github

---
+++ GRUPO_8_PFA1/src/main/java/ufide/grupo_8_pfa1/GRUPO_8_PFA1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ufide.grupo_8_pfa1;
+
+/**
+ *
+ * @author JousteenCordero(jcordero60060@ufide.ac.cr)
+ */
+public class GRUPO_8_PFA1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+    }
+}
