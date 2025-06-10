418941ab3176e4d43fb92eaf1fb1989c14504cf9
Dilan <dilanartiles@gmail.com>
2025-06-09 18:08:39 -0600

    Main sin iconos

---
+++ Proyecto.java
@@ -0,0 +1,88 @@
+package com.mycompany.proyecto;
+
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author Dilan
+ */
+public class Proyecto {
+
+    public static void main(String[] args) {
+        String [] botonesMenuPrincipal = { 
+        "Gestionar Llegada de Pacientes",
+        "Ayuda",
+        "Salir"
+        };
+        
+        while (true){
+            int menuPrincipal = Read.readOption(null,
+                    "Seleccione una Opción", 
+                    "Hospital Su Salud", 
+                    JOptionPane.DEFAULT_OPTION, 
+                    JOptionPane.INFORMATION_MESSAGE, 
+                    null, 
+                    botonesMenuPrincipal, 
+                    botonesMenuPrincipal[0]);
+    
+            switch (menuPrincipal){
+                case 0:
+                    gestionarPacientes();
+                    break;
+        
+                case 1:
+                    break;
+            
+                case 2:
+                    JOptionPane.showMessageDialog(null, "¡Gracias por usar el Sistema!");
+                    System.exit(0);
+                    break;
+            }
+        }
+    }
+    
+    public static void gestionarPacientes(){
+        String [] botonesSubMenu = {
+            "Seleccionar Ficha",
+            "Atender Paciente",
+            "Abandonar Cola",
+            "Mostrar Fichas Pendientes",
+            "Mostrar Quejas Recibidas",
+            "Regresar"
+        };
+        
+        boolean bucle = true;
+        while (bucle){
+            int subMenu = Read.readOption(null,
+                    "Seleccione una Opción", 
+                    "Hospital Su Salud", 
+                    JOptionPane.DEFAULT_OPTION, 
+                    JOptionPane.INFORMATION_MESSAGE, 
+                    null, 
+                    botonesSubMenu, 
+                    botonesSubMenu[0]);
+            
+            switch (subMenu){
+                case 0:
+                    break;
+        
+                case 1:
+                    break;
+            
+                case 2:
+                    break;
+                
+                case 3:
+                    break;
+                
+                case 4:
+                    break;
+                    
+                case 5:
+                    bucle = false;
+                    break;  
+            }
+        }
+    }  
+}
