bd56d0b97de6554aebf730b9d718e2b96137b81e
JCordero60060 <jcordero60060@ufide.ac.cr>
2025-08-18 16:46:57 -0600

    Chatbot - Clases ArbolBinario, NodoArbol, Chatbot, ListaPreguntas & Pregunta

--- GRUPO_8_PFA1/src/main/java/com/mycompany/proyecto/ListaPreguntas.java
+++ GRUPO_8_PFA1/src/main/java/com/mycompany/proyecto/ListaPreguntas.java
@@ -23,48 +23,4 @@ public class ListaPreguntas {
     }
     
     //Metodos
-    public void agregarPregunta(String codigo, String nombre, String respuesta) {
-        Pregunta nueva = new Pregunta(codigo, nombre, respuesta);
-        if (cabeza == null) {
-            cabeza = nueva;
-        } else {
-            Pregunta temp = cabeza;
-            while (temp.getSiguiente() != null) {
-                temp = temp.getSiguiente();
-            }
-            temp.setSiguiente(nueva);
-        }
-    }
-
-    public Pregunta buscarPregunta(String codigo) {
-        Pregunta temp = cabeza;
-        while (temp != null) {
-            if (temp.getCodigo().equals(codigo)) return temp;
-            temp = temp.getSiguiente();
-        }
-        return null;
-    }
-
-    public boolean eliminarPregunta(String codigo) {
-        if (cabeza == null) return false;
-
-        if (cabeza.getCodigo().equals(codigo)) {
-            cabeza = cabeza.getSiguiente();
-            return true;
-        }
-
-        Pregunta temp = cabeza;
-        while (temp.getSiguiente() != null) {
-            if (temp.getSiguiente().getCodigo().equals(codigo)) {
-                temp.setSiguiente(temp.getSiguiente().getSiguiente());
-                return true;
-            }
-            temp = temp.getSiguiente();
-        }
-        return false;
-    }
-
-    public boolean estaVacia() {
-        return cabeza == null;
-    }
 }
